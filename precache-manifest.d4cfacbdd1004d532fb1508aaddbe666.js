6751d52116728ffa422b662f9e1d798283889d78
Vodkamishka <andrtomsk@yandex.com>
2019-04-02 16:55:07 +0700

    Updates

---
+++ precache-manifest.d4cfacbdd1004d532fb1508aaddbe666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "217a123f1367c277a6f7",
+    "url": "/react-sportwear-app/static/css/main.abc65f8a.chunk.css"
+  },
+  {
+    "revision": "217a123f1367c277a6f7",
+    "url": "/react-sportwear-app/static/js/main.217a123f.chunk.js"
+  },
+  {
+    "revision": "c36c54dad73da753d65a",
+    "url": "/react-sportwear-app/static/js/runtime~main.c36c54da.js"
+  },
+  {
+    "revision": "9ae95aafab6fb15cbfb6",
+    "url": "/react-sportwear-app/static/css/2.47871f7f.chunk.css"
+  },
+  {
+    "revision": "9ae95aafab6fb15cbfb6",
+    "url": "/react-sportwear-app/static/js/2.9ae95aaf.chunk.js"
+  },
+  {
+    "revision": "c9c5470d025543f842826545efbf72e1",
+    "url": "/react-sportwear-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-sportwear-app/precache-manifest.477a04e143356e7ed5133517041c3215.js"
+  "/react-sportwear-app/precache-manifest.d4cfacbdd1004d532fb1508aaddbe666.js"
 );
 
 workbox.clientsClaim();
