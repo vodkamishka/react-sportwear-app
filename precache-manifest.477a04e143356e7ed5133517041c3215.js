4c0d43b236384f6a847400f047ab05acd8a6323c
Vodkamishka <andrtomsk@yandex.com>
2019-04-02 16:39:31 +0700

    Updates

---
+++ precache-manifest.477a04e143356e7ed5133517041c3215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd0606073495f3f12807",
+    "url": "/react-sportwear-app/static/css/main.f6ffcf5f.chunk.css"
+  },
+  {
+    "revision": "fd0606073495f3f12807",
+    "url": "/react-sportwear-app/static/js/main.fd060607.chunk.js"
+  },
+  {
+    "revision": "c36c54dad73da753d65a",
+    "url": "/react-sportwear-app/static/js/runtime~main.c36c54da.js"
+  },
+  {
+    "revision": "4d67224b3be896b0c4ee",
+    "url": "/react-sportwear-app/static/css/2.fecf0e5a.chunk.css"
+  },
+  {
+    "revision": "4d67224b3be896b0c4ee",
+    "url": "/react-sportwear-app/static/js/2.4d67224b.chunk.js"
+  },
+  {
+    "revision": "486656e88c90319b80ed219f7e95a785",
+    "url": "/react-sportwear-app/index.html"
+  }
+];
